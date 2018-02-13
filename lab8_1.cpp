8b55bc907d1fc3570c5c60ecd3258c698ed8b5ad
Unknown <farynanmonot@gmail.com>
2018-02-13 17:39:12 +0700

    ข้อ1-2

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -7,6 +7,6 @@ int main () {
         int arr[10] = {3,2,5,5,6,9,1,3,2,7};
 
         // เขียนโปรแกรมเพื่อแสดงค่าใน arr
-
+        cout << arr[0] << arr[1] << arr[2] << arr[3] << arr[4] << arr[5] << arr[6] << arr[7] << arr[8] << arr[9];
         return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,14 +4,29 @@ using namespace std;
 
 int main () {
 
-        char name[] = {"Anukul"};
+        char name[] = {"Nattha"};
         int nameLength = 6;
         cout << name;
+        char nom[7];
 
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
+        nom[0] = name[6];
+        nom[1] = name[5];
+        nom[2] = name[4];
+        nom[3] = name[3];
+        nom[4] = name[2];
+        nom[5] = name[1];
+        nom[6] = '\0';
+        cont << nom << end;
 
-
+        nom[0] = name[0] + 1;
+        nom[1] = name[1] + 1;
+        nom[2] = name[2] + 1;
+        nom[3] = name[3] + 1;
+        nom[4] = name[4] + 1;
+        nom[5] = name[5] + 1;
+        cont << nom << end;
 
         return 0;
 }
